6e4cb279da80ea749ba19525990ce8b67edc8c24
samcro85 <snowsurf85@gmail.com>
2023-12-15 08:33:51 +0100

    gestione schermate errore, caricamento e attesa

--- src/App.js
+++ src/App.js
@@ -1,36 +1,40 @@
 import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./Main";
+import Loader from "./Loader";
+import Error from "./Loader";
+import StartScreen from "./StartScreen";
 
-const initialState ={
+const initialState = {
   question: [],
 
   // 'loading', 'error', 'ready', 'active', 'finished'
-  status: 'loading'
-}
+  status: "loading",
+};
 
-const reducer = function(state, action){
-switch(action.type){
-  case 'dataReceived':
-    return {
-      ...state,
-      question: action.payload,
-      status: 'ready'
-    }
-  case 'dataFailed':
-    return {
-      ...state,
-      status: 'error'
-    }
-  default:
-    throw new Error('Invalid request')
-}
-}
+const reducer = function (state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return {
+        ...state,
+        question: action.payload,
+        status: "ready",
+      };
+    case "dataFailed":
+      return {
+        ...state,
+        status: "error",
+      };
+    default:
+      throw new Error("Invalid request");
+  }
+};
 
 function App() {
   // useReducer per creare ed aggiornare gli state
-  const [state, dispatch] = useReducer(reducer, initialState)
+  const [{ question, status }, dispatch] = useReducer(reducer, initialState);
 
+  const numQuestions = question.length
   // useEffect per fare la richiesta al server
   useEffect(function () {
     async function getData() {
@@ -39,10 +43,10 @@ function App() {
         if (!res.ok) throw new Error("INVALID REQUEST");
         const data = await res.json();
         console.log(data);
-         dispatch({type: 'dataReceived', payload: data})
+        dispatch({ type: "dataReceived", payload: data });
         console.log(data);
       } catch (e) {
-        dispatch({type: 'dataFailed'})
+        dispatch({ type: "dataFailed" });
       }
     }
     getData();
@@ -52,8 +56,10 @@ function App() {
       <Header />
 
       <Main>
-        <p>1/15</p>
-        <p>Question?</p>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && <StartScreen questions={numQuestions} />}
+
       </Main>
     </div>
   );
